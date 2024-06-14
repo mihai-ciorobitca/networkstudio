c3d2d206e5c27864a9bbcd8d7999ee50aa531f10
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-06-14 15:52:37 +0300

    Removed Pingendo code

--- api/app.py
+++ api/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/travel-time-minimization')
+def travel_time_minimization():
+    return render_template('travel-time-minimization.html')
