c3439b85e943504427666aca59f83947df1d9ff4
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-06-15 10:37:59 +0300

    Removed Pingendo code

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
@@ -9,3 +9,12 @@ def index():
 @app.route('/travel-time-minimization')
 def travel_time_minimization():
     return render_template('travel-time-minimization.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/assets/<path:filename>')
+def custom_static(filename):
+    return send_from_directory(app.template_folder + '/assets', filename)
