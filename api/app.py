3d0401f8e92c752caa48b1508bb44e000058f2b0
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-06-17 21:00:40 +0300

    Removed Pingendo code

--- api/app.py
+++ api/app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, send_from_directory, redirect
 
 app = Flask(__name__, template_folder='templates')
 
-links = {
-    "Mr. Robot Season 1 Episode 1": "https://jumpshare.com/embed/3YfW74SSdJQ8rjFRbVf6",
-    "Mr. Robot Season 1 Episode 2": "https://jumpshare.com/embed/7stUTPqXwLby4vekXRSD",
-    "Mr. Robot Season 1 Episode 3": "https://jumpshare.com/embed/nYu50wipdpx2CHccn65i",
+routes = {
+    "Mr.Robot/Season1/Episode1": "https://jumpshare.com/embed/3YfW74SSdJQ8rjFRbVf6",
+    "Mr.Robot/Season1/Episode2": "https://jumpshare.com/embed/7stUTPqXwLby4vekXRSD",
+    "Mr.Robot/Season1/Episode3": "https://jumpshare.com/embed/nYu50wipdpx2CHccn65i",
+    "Mr.Robot/Season1/Episode4": "https://jumpshare.com/embed/xTb55Cuco8EGwhDk63s8",
 }
 
 @app.route('/')
@@ -16,10 +17,6 @@ def index():
 def login():
     return render_template('login.html')
 
-@app.route('/videos')
-def videos():
-    return render_template('videos.html', links=links.values())
-
 @app.route('/travel-time-minimization')
 def travel_time_minimization():
     return redirect('https://travel-time-minimization.vercel.app')
@@ -27,3 +24,10 @@ def travel_time_minimization():
 @app.route('/assets/<path:filename>')
 def custom_static(filename):
     return send_from_directory(app.template_folder + '/assets', filename)
+
+def create_route(endpoint, url):
+    def generic_route():
+        return render_template('video.html', url=url)
+    app.add_url_rule(f'/{endpoint}', endpoint, generic_route)
+
+[create_route(endpoint, url) for endpoint, url in routes.items()]
