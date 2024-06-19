83d949fc05fd24b5fbc22d6217148339f14d0d7b
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-06-19 18:45:49 +0300

    Removed Pingendo code

--- api/app.py
+++ api/app.py
@@ -3,11 +3,13 @@ from flask import Flask, render_template, send_from_directory, redirect
 app = Flask(__name__, template_folder='templates')
 
 routes = {
-    "Mr.Robot/Season1/Episode1": "https://jumpshare.com/embed/3YfW74SSdJQ8rjFRbVf6",
-    "Mr.Robot/Season1/Episode2": "https://jumpshare.com/embed/7stUTPqXwLby4vekXRSD",
-    "Mr.Robot/Season1/Episode3": "https://jumpshare.com/embed/nYu50wipdpx2CHccn65i",
-    "Mr.Robot/Season1/Episode4": "https://jumpshare.com/embed/xTb55Cuco8EGwhDk63s8",
-    "Mr.Robot/Season1/Episode5": "https://drive.google.com/file/d/1Vbrb7kcoVaUjaYeJYHoLzJzyDIzLSsmM/preview"
+    "Mr.Robot/Season1/Episode1": "https://drive.google.com/file/d/1APwSSGQ_QIg-SmmmxhnZm8xx6AFR9yQf/preview",
+    "Mr.Robot/Season1/Episode2": "https://drive.google.com/file/d/11Dkz-7afKeT8GCa0aoYoZQ4z5iZnMe7B/preview",
+    "Mr.Robot/Season1/Episode3": "https://drive.google.com/file/d/1XJ7zoFwyju2ksBa98IgMb724lZb3PQiQ/preview",
+    "Mr.Robot/Season1/Episode4": "https://drive.google.com/file/d/1oGCDrpOtR-1pyqVq-dE5lwM8fVG8PSH8/preview",
+    "Mr.Robot/Season1/Episode5": "https://drive.google.com/file/d/1HOblgXpeyWQ2AJTizBjrabTt9oaiC-PL/preview",
+    "Mr.Robot/Season1/Episode6": "https://drive.google.com/file/d/1juTMfI_L_kcQsqsX4yB2Xolc5hHTUn_q/preview",
+
 }
 
 @app.route('/')
