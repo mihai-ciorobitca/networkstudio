0c4434db5e697fd8837ee48ec71cbb2f402a09bf
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-06-29 16:23:13 +0300

    Removed Pingendo code

--- api/app.py
+++ api/app.py
@@ -25,10 +25,36 @@ def create_route(endpoint, url):
         return render_template('video.html', url=url)
     app.add_url_rule(f'/{endpoint}', endpoint, generic_route)
 
-@app.route('/videos/<name>/<season>/<episode>')
+@app.route('/videos/', defaults={'name': None, 'season': None, 'episode': None})
+@app.route('/videos/<name>/', defaults={'season': None, 'episode': None})
+@app.route('/videos/<name>/<season>/', defaults={'episode': None})
+@app.route('/videos/<name>/<season>/<episode>/')
 def video_route(name, season, episode):
-    response = supabase_client.table('movies').select('*').eq('name', name).eq('season', season).eq('episode', episode).execute()
+    query = supabase_client.table('movies').select('*')
+    
+    if name:
+        query = query.eq('name', name)
+
+    if season:
+        query = query.eq('season', season)
+        
+    if episode:
+        query = query.eq('episode', episode)
+        
+    response = query.execute()
+    
     if len(response.data) == 0:
         return render_template('page_404.html')
-    url = response.data[0]['link']
-    return render_template('video.html', url=url)
+    
+    if not name and not season and not episode:
+        names = list(set(map(lambda x: x["name"], response.data)))
+        return render_template('names.html', names=names)
+    elif not season and not episode:
+        seasons = list(set(map(lambda x: x["season"], response.data)))
+        return render_template('seasons.html', seasons=seasons, name=name)
+    elif season and not episode:
+        episodes = list(set(map(lambda x: x["episode"], response.data)))
+        return render_template('episodes.html', episodes=episodes, name=name, season=season)
+    else:
+        url = response.data[0]['link']
+        return render_template('video.html', url=url)
