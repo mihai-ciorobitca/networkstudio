5e2d0d7356aaf08aeb01d7ec54cbda97528ee814
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-07-06 22:04:04 +0300

    Removed Pingendo code

--- api/app.py
+++ api/app.py
@@ -29,38 +29,41 @@ def login():
 def travel_time_minimization():
     return redirect('https://travel-time-minimization.vercel.app')
 
-@app.route('/videos/', defaults={'name': None, 'season': None, 'episode': None})
-@app.route('/videos/<name>/', defaults={'season': None, 'episode': None})
-@app.route('/videos/<name>/<season>/', defaults={'episode': None})
-@app.route('/videos/<name>/<season>/<episode>/')
+@app.route('/videos/', defaults={'name': None, 'season': None, 'episode': None, 'part': None})
+@app.route('/videos/name/<name>/', defaults={'season': None, 'episode': None, 'part': None})
+@app.route('/videos/name/<name>/part/<part>/', defaults={'season': None, 'episode': None})
+@app.route('/videos/name/<name>/season/<season>/', defaults={'episode': None, 'part': None})
+@app.route('/videos/name/<name>/season/<season>/episode/<episode>/', defaults={'part': None})
 @cache.cached(timeout=10)
-def video_route(name, season, episode):
+def video_route(name, season, episode, part):
     query = supabase_client.table('movies').select('*')
     
     if name:
         query = query.eq('name', name)
-
-    if season:
-        query = query.eq('season', season)
-        
-    if episode:
-        query = query.eq('episode', episode)
-        
+        if season:
+            query = query.eq('season', season)
+            if episode:
+                query = query.eq('episode', episode)
+                response = query.execute()
+                return render_template('video.html', name=name, season=season, episode=episode, url=response.data[0]['link'])
+            response = query.execute()
+            episodes = sorted(list(set(map(lambda x: x["episode"], response.data))))
+            return render_template('episodes.html', episodes=episodes, name=name, season=season)
+        if part:
+            query = query.eq('part', part)
+            response = query.execute()
+            return render_template('video.html', name=name, part=part, url=response.data[0]['link'])
+        response = query.execute()
+        if response.data[0].get('season') is not None:
+            seasons = sorted(list(set(map(lambda x: x["season"], response.data))))
+            return render_template('seasons.html', seasons=seasons, name=name)
+        parts = sorted(list(set(map(lambda x: x["part"], response.data))))
+        return render_template('parts.html', parts=parts, name=name)
     response = query.execute()
+    names = sorted(list(set(map(lambda x: x["name"], response.data))))
+    return render_template('names.html', names=names)
     
-    if len(response.data) == 0:
-        return render_template('page_404.html')
+
     
-    if not name and not season and not episode:
-        names = list(set(map(lambda x: x["name"], response.data)))
-        return render_template('names.html', names=sorted(names))
-    elif not season and not episode:
-        seasons = list(set(map(lambda x: x["season"], response.data)))
-        return render_template('seasons.html', seasons=sorted(seasons), name=name)
-    elif season and not episode:
-        episodes = list(set(map(lambda x: x["episode"], response.data)))
-        return render_template('episodes.html', episodes=sorted(episodes), name=name, season=season)
-    else:
-        url = response.data[0]['link']
-        print(url)
-        return render_template('video.html', url=url)
+
+    
