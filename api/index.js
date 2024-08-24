9b3bceeee19486f419dd1956b52c4b06c3aef033
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-08-24 12:28:35 +0300

    m

--- api/index.js
+++ api/index.js
@@ -16,6 +16,8 @@ const cache = apicache.middleware;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   // Homepage
   res.render('index');
